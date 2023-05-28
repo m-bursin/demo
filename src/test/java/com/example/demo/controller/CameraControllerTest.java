44fddec6f512543b02f9e3ed03bd192a79f20920
Your Name <m.bursin@mail.ru>
2023-05-28 12:20:23 +0500

    commit

---
+++ src/test/java/com/example/demo/controller/CameraControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import com.example.demo.DemoApplication;
+import com.example.demo.converter.CameraRawCameraSourceCameraTokenToCameraConverter;
+import com.example.demo.service.CameraService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(CameraController.class)
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = DemoApplication.class)
+public class CameraControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    CameraService cameraService;
+
+    @SpyBean
+    CameraRawCameraSourceCameraTokenToCameraConverter converter;
+
+    @Test
+    public void getCameras() throws Exception {
+        mockMvc.perform(get("/get-cameras"))
+                .andExpect(status().isOk());
+    }
+}
